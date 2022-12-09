5ab0ba2cf0d66eec2cb88f23b5c80d6c645da64e
fusaroli <fusaroli@gmail.com>
2022-12-09 10:43:37 +0100

    minor coding style consistency edits

--- R/Phase_delay_simulation.R
+++ R/Phase_delay_simulation.R
@@ -29,7 +29,7 @@
 Phase_delay_simulation <- function(n,b, latency_mean, latency_sd, duration_mean, duration_sd, cutoff){
   require(dplyr)
 
-  IOI_1 <- dplyr::tibble(Interval = rnorm(n, mean = latency_mean , sd= latency_sd),
+  IOI_1 <- dplyr::tibble(Interval = rnorm(n, mean = latency_mean , sd = latency_sd),
                          Duration = abs(rnorm(n, mean = duration_mean,  sd = duration_sd)),
                          Onset = NA,
                          Offset = NA,
@@ -38,7 +38,7 @@ Phase_delay_simulation <- function(n,b, latency_mean, latency_sd, duration_mean,
     dplyr::mutate(Onset = Interval, Offset = Onset + Duration)
 
 
-  IOI_2 <- dplyr::tibble(Interval = rnorm(n, mean = latency_mean , sd= latency_sd),
+  IOI_2 <- dplyr::tibble(Interval = rnorm(n, mean = latency_mean , sd = latency_sd),
                          Duration = abs(rnorm(n, mean = duration_mean, sd = duration_sd)),
                          Onset = NA,
                          Offset = NA,
@@ -46,11 +46,11 @@ Phase_delay_simulation <- function(n,b, latency_mean, latency_sd, duration_mean,
                          CallNr = 1:n) %>%
     dplyr::mutate(Onset = Interval, Offset = Onset + Duration)
 
-  for (i in 2:n){
-    IOI_1$Onset[i] = IOI_1$Offset[i-1] + IOI_1$Interval[i]
+  for (i in 2:n) {
+    IOI_1$Onset[i] = IOI_1$Offset[i - 1] + IOI_1$Interval[i]
     IOI_1$Offset[i] = IOI_1$Onset[i] + IOI_1$Duration[i]
 
-    IOI_2$Onset[i] = IOI_2$Offset[i-1] + IOI_2$Interval[i]
+    IOI_2$Onset[i] = IOI_2$Offset[i - 1] + IOI_2$Interval[i]
     IOI_2$Offset[i] = IOI_2$Onset[i] + IOI_2$Duration[i]
 
   }
@@ -65,10 +65,10 @@ Phase_delay_simulation <- function(n,b, latency_mean, latency_sd, duration_mean,
   # NOW LOOP THROUGH FIND ALL PLACES WHERE ONSET LATENCY IS LOW BUT OFFSET LATENCY IS NEGATIVE. (THIS MEANS OVERLAP BUT STARTED CLOSELY TO EACHOTHER)
   # LET THE BIRD SING AT TIME = T BUT ADJUST THEIR NEXT RESPONSE TIMING AT (T+1)
 
-  for (i in 1:nrow(IOI_combined)){
-    if (IOI_combined$Overlapping[i] == 0 & IOI_combined$Onset_Latency[i] < cutoff){ #Check if persons call are overlapping and elgible for phase correction.
+  for (i in 1:nrow(IOI_combined)) {
+    if (IOI_combined$Overlapping[i] == 0 & IOI_combined$Onset_Latency[i] < cutoff) { #Check if persons call are overlapping and elgible for phase correction.
 
-      Call_to_adjust <- which(IOI_combined$ID == IOI_combined$ID[i] & IOI_combined$CallNr == IOI_combined$CallNr[i]+1) #Find the next call of the person who are going to phase correct.
+      Call_to_adjust <- which(IOI_combined$ID == IOI_combined$ID[i] & IOI_combined$CallNr == IOI_combined$CallNr[i] + 1) #Find the next call of the person who are going to phase correct.
 
       Phase_correction <-  rnorm(1, mean = IOI_combined$Onset_Latency[i], sd = b * IOI_combined$Onset_Latency[i]) # Phase correction
 
@@ -85,13 +85,13 @@ Phase_delay_simulation <- function(n,b, latency_mean, latency_sd, duration_mean,
 
       ### Adjust the remaining calls for the phase correction ###
 
-      for (ii in min(nrow(IOI_combined),1+Call_to_adjust):nrow(IOI_combined)){
+      for (ii in min(nrow(IOI_combined), 1 + Call_to_adjust):nrow(IOI_combined)) {
         #On-, Offset
-        IOI_combined$Onset[ii] <- IOI_combined$Offset[ii-1] + IOI_combined$Interval[ii]
+        IOI_combined$Onset[ii] <- IOI_combined$Offset[ii - 1] + IOI_combined$Interval[ii]
         IOI_combined$Offset[ii] <- IOI_combined$Onset[ii] + IOI_combined$Duration[ii]
         #On, Offset Latencies
-        IOI_combined$Onset_Latency[ii] <- IOI_combined$Onset[ii] - IOI_combined$Onset[ii-1]
-        IOI_combined$Offset_Latency[ii] <- IOI_combined$Offset[ii] - IOI_combined$Offset[ii-1]
+        IOI_combined$Onset_Latency[ii] <- IOI_combined$Onset[ii] - IOI_combined$Onset[ii - 1]
+        IOI_combined$Offset_Latency[ii] <- IOI_combined$Offset[ii] - IOI_combined$Offset[ii - 1]
 
         IOI_combined$Overlapping[ii] <- ifelse(IOI_combined$Offset_Latency[ii] <= 0 , 1, 0)
 
@@ -102,7 +102,7 @@ Phase_delay_simulation <- function(n,b, latency_mean, latency_sd, duration_mean,
 
 }
 
-ok <- Phase_delay_simulation(n = 150, b = 1/10, latency_mean = 5, latency_sd = 2, duration_mean = 15, duration_sd = 4, cutoff =4)
+ok <- Phase_delay_simulation(n = 150, b = 1/10, latency_mean = 5, latency_sd = 2, duration_mean = 15, duration_sd = 4, cutoff = 4)
 
 
 
